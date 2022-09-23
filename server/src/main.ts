6f30b734eed1274ca291b797eef37bf1ccc65502
mingi <skyup9393@gmail.com>
2022-09-23 21:28:01 +0900

    Update env and dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,17 +3,12 @@ import { AppModule } from './app.module';
 import compression from 'compression';
 import * as ip from 'ip';
 import * as dotenv from 'dotenv';
-import * as path from 'path';
 
-dotenv.config({
-  path: path.resolve(
-    process.env.NODE_ENV === 'production' ? '.prod.env' : '.dev.env',
-  ),
-});
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PROTOCOL = 'http';
+  const PROTOCOL = process.env.PROTOCOL;
   const myIp = ip.address();
   const PORT = process.env.PORT;
 
@@ -21,6 +16,7 @@ async function bootstrap() {
   app.use(compression());
 
   await app.listen(PORT);
-  console.log(`Server running: ${PROTOCOL}://${myIp}:${PORT}`);
+
+  console.log(`HANGANGGAK Server running: ${PROTOCOL}://${myIp}:${PORT}`);
 }
 void bootstrap();
