22626ab01c7efc8b60afd0a24d478617b336eff4
reemrev21 <reemrev21@gmail.com>
2022-09-25 14:25:51 +0900

    [feature] #6 setting react-query & remove useless file

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
+import {QueryClient, QueryClientProvider} from "react-query";
+import React from "react";
 
 const Map = dynamic(() => import("../components/map"), { ssr: false });
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Map><Component {...pageProps} /></Map>;
+  return (
+      <QueryClientProvider client={queryClient}>
+        <Map>
+          <Component {...pageProps} />
+        </Map>
+      </QueryClientProvider>
+  );
 }
 
 export default MyApp;
