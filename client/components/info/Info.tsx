0591831d58e10ae541feabe943bb0ef0cd7ea0ad
byungmin12 <kbm940526@naver.com>
2022-10-01 01:55:31 +0900

    [feature] #16 map component and marker
    테스트를 위해 주석처리함

--- client/components/info/Info.tsx
+++ client/components/info/Info.tsx
@@ -1,24 +1,24 @@
-import React, {ChangeEvent, useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {useHanRiverInfo} from "../../api/info";
 import {useQuery} from "react-query";
 
 const Info = () => {
-  const { data: search } = useQuery<string>('search',() => '', {
-    staleTime: Infinity,
-  });
-  const {isError, isLoading, data: hanRiverData} = useHanRiverInfo(search)
+    const {data: search} = useQuery<string>('search', () => '', {
+        staleTime: Infinity,
+    });
+    const {isError, isLoading, data: hanRiverData} = useHanRiverInfo(search)
 
-  useEffect(() => {
+    useEffect(() => {
 
-  }, [search])
+    }, [search])
 
-  return (
-    <div>
-      {isLoading && <>로딩 중 입니다.</>}
-      {isError && <>에러가 발생했습니다.</>}
-      {hanRiverData? <>{hanRiverData.congestion.장소혼잡도메시지}</> : <div>none</div>}
-    </div>
-  )
+    return (
+        <div>
+            {isLoading && <>로딩 중 입니다.</>}
+            {isError && <>에러가 발생했습니다.</>}
+            {/*{hanRiverData? <>{hanRiverData.congestion.장소혼잡도메시지}</> : <div>none</div>}*/}
+        </div>
+    )
 }
 
 export default Info;
