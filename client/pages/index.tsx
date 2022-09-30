3e77169683da3e5fbf4caa3df0350c025f26b36f
byungmin12 <kbm940526@naver.com>
2022-10-01 01:56:16 +0900

    [feature] #16 map component and marker
    마커 추가
    
    - 좌표 위치 조정해야함

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,10 +4,12 @@ import Info from "../components/info/Info";
 import MapContext from "../components/map/mapContext";
 import React from "react";
 import Overlay from "ol/Overlay";
+import {useHanRiverInfo} from "../api/info";
+import styled, {keyframes} from "styled-components";
 
 const locatainData = [{
     name: "뚝섬",
-    position: [1202.5006305099564, 451.5764799331531]
+    position: [1202.5006305099564, 491.5764799331531]
 }, {
     name: "망원",
     position: [692.3623600395065, 536.6586097266725]
@@ -19,31 +21,50 @@ const locatainData = [{
     position: [886.9668608547381, 395.89365001267686]
 }, {
     name: "잠실",
-    position: [1233.3108189961722, 413.73792151037173]
-}, {
-    name: "여의도",
-    position: [789.4961078458748, 461.9891656283356]
-},]
+    position: [1233.3108189961722, 433.73792151037173]
+},
+//     {
+//     name: "여의도",
+//     position: [789.4961078458748, 461.9891656283356]
+// },
+]
 
 const Home: NextPage = () => {
     const {map} = React.useContext(MapContext);
+    const {isError, isLoading, data: hanRiverData} = useHanRiverInfo("")
+
 
     React.useEffect(() => {
-        if (map) {
+        if (map && hanRiverData) {
             locatainData.forEach((data) => {
-                const tag = document.createElement("div")
-                tag.innerText = data.name
-                const marker = new Overlay({
-                    position: data.position,
-                    element: tag,
+
+                const textTag = document.createElement("div")
+                textTag.innerText = data.name
+                textTag.className = "label"
+
+                const pulseTag = document.createElement("div")
+                pulseTag.className = `pulse ${hanRiverData[data.name].congestionMessage}`
+                pulseTag.appendChild(textTag)
+
+
+                // const textMarker = new Overlay({
+                //     position: data.position,
+                //     element: textTag,
+                // });
+                const pulseMarker = new Overlay({
+                    position: [data.position[0], data.position[1]],
+                    element: pulseTag,
                 });
-                map.addOverlay(marker)
+
+                // map.addOverlay(textMarker)
+                map.addOverlay(pulseMarker)
+
             })
         }
-    }, [map])
+    }, [map, hanRiverData])
 
     return (
-        <div style={{width: "100vw", height: "100vh"}}>
+        <Wrapper>
             <div id="map"
                  style={{width: "100vw", height: "100vh", position: "fixed", top: "0", left: "0"}}>
             </div>
@@ -52,10 +73,90 @@ const Home: NextPage = () => {
                 <div style={{marginTop: "30px"}}/>
                 <Info/>
             </div>
-            <div>test</div>
-        </div>
+        </Wrapper>
 
     );
 };
+
+const pulseAnimation = keyframes`
+  0% {
+    transform: scale(0.1, 0.1);
+    opacity: 0.0;
+  }
+
+  50% {
+    opacity: 1.0;
+
+  }
+  100% {
+    transform: scale(1.2, 1.2);
+    opacity: 0;
+  }
+`
+
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  .pulse {
+    border-radius: 50%;
+    height: 14px;
+    width: 14px;
+    position: absolute;
+    left: 80%;
+    top: 50%;
+    margin: 11px 0px 0px -12px;
+    transform: rotateX(55deg);
+    z-index: -2;
+
+    :after {
+      content: "";
+      border-radius: 50%;
+      height: 40px;
+      width: 40px;
+      position: absolute;
+      margin: -13px 0 0 -13px;
+      animation: ${pulseAnimation} 1s ease-out;
+      animation-iteration-count: infinite;
+      opacity: 0.0;
+      animation-delay: 1.1s;
+    }
+  }
+
+  .여유 {
+    background: rgba(0, 255, 0, 0.4);
+
+    :after {
+      box-shadow: 0 0 1px 2px green;
+    }
+  }
+
+  .보통 {
+    background: rgba(0, 0, 255, 0.4);
+
+    :after {
+      box-shadow: 0 0 1px 2px blue;
+    }
+  }
+
+  .혼잡 {
+    background: rgba(255, 0, 0, 0.4);
+
+    :after {
+      box-shadow: 0 0 1px 2px red;
+    }
+  }
+
+  .label {
+    position: absolute;
+    top: -25px;
+    left: -8px;
+    color: #111111;
+    font-weight: bold;
+    white-space: nowrap;
+  }
+`
+
 
 export default Home;
