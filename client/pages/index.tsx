17949227aa212e534b8cd76a31b17bd471fd8018
byungmin12 <kbm940526@naver.com>
2022-09-30 12:21:47 +0900

    [feature] #16 map component and marker
    지도 이미지로 변경하면서 지도 크기 변경

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,22 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 import Search from "../components/search/Search";
 import Info from "../components/info/Info";
 
 const Home: NextPage = () => {
 
-  return(
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <div id="map" style={{ width: "70vw", height: "100vh", float: "left" }}>
-      </div>
-      <div  style={{ display: "flex", flexDirection: "column", width: "30vw", height: "100vh", float: "right" }}>
-        <Search />
-        <div style={{ marginTop: "30px"}}/>
-        <Info />
-      </div>
-    </div>
+    return (
+        <div style={{width: "100vw", height: "100vh"}}>
+            <div id="map"
+                 style={{width: "100vw", height: "100vh", position: "fixed", top: "0", left: "0"}}>
+            </div>
+            <div style={{display: "flex", flexDirection: "column", width: "30vw", height: "100vh", float: "right"}}>
+                <Search/>
+                <div style={{marginTop: "30px"}}/>
+                <Info/>
+            </div>
+        </div>
 
-);
+    );
 };
 
 export default Home;
