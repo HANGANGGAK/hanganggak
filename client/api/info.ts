1773077a24346b859ae6eeab4112958b761c91d4
byungmin12 <kbm940526@naver.com>
2022-10-01 01:56:52 +0900

    [feature] #16 map component and marker
    마카 렌더에서도 값이 필요하여 살짝 수정함
    
    승민민꺼 머지할때 승민님껄러 사용하면됩니다.

--- client/api/info.ts
+++ client/api/info.ts
@@ -1,15 +1,15 @@
-import { useQuery } from "react-query";
+import {useQuery} from "react-query";
 import api from "./api";
 
 export const fetchHanRiverInfo = async (search: string | undefined) => {
-  if (search === "" || search === undefined) {
-    return null
-  } else {
-    const { data } = await api.get(`/han-river/${search}`)
-    return data
-  }
+    if (search === undefined) {
+        return null
+    } else {
+        const {data} = await api.get(`http://43.200.191.202:2209/han-river/${search}`)
+        return data
+    }
 }
 
-export const useHanRiverInfo =  (search : string | undefined) => {
-  return useQuery(['useHanRiverInfo', search],() => fetchHanRiverInfo(search))
+export const useHanRiverInfo = (search: string | undefined) => {
+    return useQuery(['useHanRiverInfo', search], () => fetchHanRiverInfo(search))
 }
