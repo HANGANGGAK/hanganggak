df79be33d54d8d665785f26b07dbc3ff60de0c13
byungmin12 <kbm940526@naver.com>
2022-10-01 01:55:09 +0900

    [feature] #16 map component and marker
    스타일드 컴포넌트 설치

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        styledComponents: {
+            displayName: true,
+            ssr: true,
+            fileName: true,
+            cssProp: true,
+            pure: true,
+        },
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
