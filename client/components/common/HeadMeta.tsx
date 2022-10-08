d52c78e848e6428d6f8937674292eeee9ab01d3f
reemrev21 <reemrev21@gmail.com>
2022-10-08 18:09:32 +0900

    [fix] description 수정

--- client/components/common/HeadMeta.tsx
+++ client/components/common/HeadMeta.tsx
@@ -17,24 +17,21 @@ export const HeadMeta = ({  description, url }: IMetaType) => {
             <meta
                 name="description"
                 content={
-                    `한강각, 한강, 한강공원, 불꽃축제, 잠실 한강공원, 뚝섬 한강공원, 반포 한강공원, 이촌 한강공원, 여의도 한강공원,
-                    한강간맥, 한강 날씨, 한강공원 날씨, 뚝섬 유원지, 한강치맥, 한강날씨`
+                    `한강각, 한강, 한강공원, 불꽃축제, 잠실, 뚝섬, 반포, 이촌, 여의도 한강공원, 한강간맥, 한강 날씨, 뚝섬유원지, 한강치맥`
                 }
             />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta property="og:title" content={`한강각 ${ title !== "" ? ` || ${title} 한강공원` : ""}`} />
             <meta property="og:type" content="website" />
             <meta property="og:url" content={url || "http://hanganggak.site"} />
-            <meta property="og:description" content="한강각, 한강, 한강공원, 불꽃축제, 잠실 한강공원, 뚝섬 한강공원, 반포 한강공원, 이촌 한강공원, 여의도 한강공원,
-              한강간맥, 한강 날씨, 한강공원 날씨, 뚝섬 유원지, 한강치맥, 한강날씨" />
+            <meta property="og:description" content="한강각, 한강, 한강공원, 불꽃축제, 잠실, 뚝섬, 반포, 이촌, 여의도 한강공원, 한강간맥, 한강 날씨, 뚝섬유원지, 한강치맥" />
             <meta property="og:site_name" content="한강각" />
             <meta property="og:image" content={"https://github.com/HANGANGGAK/hanganggak/blob/dev/client/public/logo.png?raw=true"} />
             <meta property="og:article:author" content="각스튜디오 || 한강각" />
 
             <meta name="twitter:card" content="한강각" />
             <meta name="twitter:title" content={`한강각 ${ title !== "" ? ` || ${title} 한강공원` : ""  }`} />
-            <meta name="twitter:description" content="한강각, 한강, 한강공원, 불꽃축제, 잠실 한강공원, 뚝섬 한강공원, 반포 한강공원, 이촌 한강공원, 여의도 한강공원,
-                한강간맥, 한강 날씨, 한강공원 날씨, 뚝섬 유원지, 한강치맥, 한강날씨" />
+            <meta name="twitter:description" content="한강각, 한강, 한강공원, 불꽃축제, 잠실, 뚝섬, 반포, 이촌, 여의도 한강공원, 한강간맥, 한강 날씨, 뚝섬유원지, 한강치맥" />
             <meta name="twitter:image" content="https://github.com/HANGANGGAK/hanganggak/blob/dev/client/public/logo.png?raw=true" />
             <meta name="twitter:site" content="http://hanganggak.site" />
             <meta http-equiv="Copyright" content="hanganggak@gmail.com" />
