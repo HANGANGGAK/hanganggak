5d079afe4e42d28bdbd65285cb423c55f0b8c0d4
reemrev21 <reemrev21@gmail.com>
2022-10-27 11:29:25 +0900

    [feature] middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,8 +1,9 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log(req.headers);
+
   if (req.headers.get("x-forwarded-proto") !== "https") {
-    console.log(req.headers);
     return NextResponse.redirect(
       `https://${req.headers.get("host")}${req.nextUrl.pathname}`,
       301
