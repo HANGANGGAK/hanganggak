7736ddf5bd69811bd7080ebfb1a70f6d4b9cdade
reemrev21 <reemrev21@gmail.com>
2022-10-27 10:28:39 +0900

    [feature] middleware 위치 변경

--- client/_middleware.ts
+++ client/_middleware.ts
@@ -1,7 +1,8 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export function _middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.headers.get("x-forwarded-proto") !== "https") {
+    console.log(req.headers);
     return NextResponse.redirect(
       `https://${req.headers.get("host")}${req.nextUrl.pathname}`,
       301
