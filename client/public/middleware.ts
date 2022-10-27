8cdf0343c54e0d7153b22821024232bc515f8c25
reemrev21 <reemrev21@gmail.com>
2022-10-27 10:35:38 +0900

    [feature] middleware 수정

--- client/public/middleware.ts
+++ client/public/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export function _middleware(req: NextRequest, ev: NextFetchEvent) {
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.headers.get("x-forwarded-proto") !== "https") {
     console.log(req.headers);
     return NextResponse.redirect(
