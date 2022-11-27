4e1cbe94e895241d8a3870c64a85cd5e81e130fe
reemrev21 <reemrev21@gmail.com>
2022-11-27 18:46:08 +0900

    [fix] next conf 수정

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true",
 });
 
 // const nextConfig = {
@@ -15,26 +14,29 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
-    sassOptions: {
-        includePaths: [path.join(__dirname, "styles")]
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        destination: `${process.env.API_SERVER_URL}`,
+        source: "/:path*",
+      },
+    ];
+  },
+  swcMinify: true,
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      cssProp: true,
+      pure: true,
     },
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                destination: `${process.env.API_SERVER_URL}`,
-                source: "/:path*"
-            },
-        ];
-    },
-    swcMinify: true,
-    compiler: {
-        styledComponents: {
-            displayName: true,
-            ssr: true,
-            fileName: true,
-            cssProp: true,
-            pure: true,
-        },
-    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 });
